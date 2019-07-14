57f2b2a78eb8b210006d1aa5000a192f8d5dbbd0
Mengkko <aappcc1@icloud.com>
2019-07-14 17:04:08 +0900

    print function

---
+++ section02-1.py
@@ -0,0 +1,23 @@
+# section02-1
+# 파이썬 기초 코팅
+# print 구문의 이해
+
+# 기본 출력
+print('Hello Python!')
+print("Hello Python!")
+print("""Hello Python!""")
+print('''Hello Python!''')
+
+print()
+
+# Separator 옵션 사용
+print('T', 'E', 'S', 'T', sep='')
+print('2019', '07', '14', sep='-')
+print('niceman', 'google.com', sep='@')
+
+print()
+
+# end 옵션 사용
+print('Welcome To', end=' ')
+print('the black parade', end=' ')
+print('piano notes')
