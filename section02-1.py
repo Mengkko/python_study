1942243117d72e461df556b00cfe011e03b584c9
Mengkko <aappcc1@icloud.com>
2019-07-14 19:27:51 +0900

    print function 3

--- section02-1.py
+++ section02-1.py
@@ -1,3 +1,24 @@
+# Section02-1
+# 파이썬 기초 코딩
+# Print 구문의 이해
+# 참조 : https://www.python-course.eu/python3_formatted_output.php
+
+"""
+참고 : Escape 코드
+
+\n : 개행
+\t : 탭
+\\ : 문자
+\' : 문자
+\" : 문자
+\r : 캐리지 리턴
+\f : 폼 피드
+\a : 벨 소리
+\b : 백 스페이스
+\000 : 널 문자
+...
+
+"""
 # section02-1
 # 파이썬 기초 코팅
 # print 구문의 이해
@@ -28,7 +49,13 @@ print()
 print('{} and {}'.format('You', 'Me'))
 print('{0} and {1} and {0}'.format('You', 'Me'))
 print('{a} and {b}'.format(a='You', b='Me'))
-print("%s's favorite number is %d" % ('Eunki', 7))
+
 # %s: 문자, %d: 정수, %f: 실수
+print("%s's favorite number is %d" % ('Eunki', 7))
 
 print()
+
+# %d, %f, %s
+print("Test1: %5d, Price: %4.2f" % (776, 6534.123))
+print("Test1: {0:5d}, Price:{1:4.2f}".format(776, 6534.123))
+print("Test1: {a: 5d}, Price:{b: 4.2f}".format(a=776, b=6534.123))
